25aadd43106d5cec35370498e9796176829cc2b6
Tim Burt <tim@dorset-digital.net>
2018-06-20 17:14:22 +0100

    Move extension to Page Add rules to YML to try and be last in line Add checking on existing tags to attempt to remove existing canonical tags

--- src/SiteTreeCanonicalExtension.php
+++ src/SiteTreeCanonicalExtension.php
@@ -8,23 +8,37 @@ use SilverStripe\CMS\Model\SiteTreeExtension;
 class SiteTreeCanonicalExtension extends SiteTreeExtension
 {
 
-    public function MetaTags(&$tags)
-    {
+ public function MetaTags(&$tags)
+ {
 
-        $siteConfig = SiteConfig::current_site_config();
-        if ($siteConfig->CanonicalDomain != '') {
-            $canonicalBase = trim($siteConfig->CanonicalDomain, '/');
-            if (method_exists($this->owner, 'CanonicalLink')) {
-                $link = $this->owner->CanonicalLink();
-            } else {
-                $link = $this->owner->Link();
-            }
-            $canonLink = $canonicalBase . $link;
-            $atts = [
-                'rel' => 'canonical',
-                'href' => $canonLink
-            ];
-            $tags .= "\n" . HTML::createTag('link', $atts) . "\n";
-        }
-    }
+  $siteConfig = SiteConfig::current_site_config();
+  if ($siteConfig->CanonicalDomain != '') {
+   $canonicalBase = trim($siteConfig->CanonicalDomain, '/');
+   if (method_exists($this->owner, 'CanonicalLink')) {
+    $link = $this->owner->CanonicalLink();
+   } else {
+    $link = $this->owner->Link();
+   }
+   $canonLink = $canonicalBase . $link;
+   $atts = [
+       'rel' => 'canonical',
+       'href' => $canonLink
+   ];
+   $canonTag = HTML::createTag('link', $atts);
+
+   $tagsArray = explode(PHP_EOL, $tags);
+   $tagPattern = 'rel="canonical"';
+
+   $tagSearch = function ( $val ) use ( $tagPattern ) {
+    return ( stripos($val, $tagPattern) !== false ? true : false );
+   };
+
+   $currentTags = array_filter($tagsArray, $tagSearch);
+   $cleanedTags = array_diff($tagsArray, $currentTags);
+
+   $cleanedTags[] = $canonTag;
+
+   $tags = implode(PHP_EOL, $cleanedTags);
+  }
+ }
 }
