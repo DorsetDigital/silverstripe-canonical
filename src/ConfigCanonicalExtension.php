bc67efbdc01b1153262d880323cd077cec85d783
Lukas Erni <le@kraftausdruck.ch>
2019-11-01 12:37:09 +0100

    add canonical URL on Page basis, dynamic values from CanonicalLink() can be with or without base (protocol, domain), add german translation, remove comment about trailing slash in description - it is removed in code anyway, make use of placeholder

--- src/ConfigCanonicalExtension.php
+++ src/ConfigCanonicalExtension.php
@@ -14,8 +14,13 @@ class ConfigCanonicalExtension extends DataExtension
     ];
     
     public function updateCMSFields(FieldList $fields) {        
-        $fields->addFieldToTab('Root.Canonical', LiteralField::create('Info', '<p>'._t(__CLASS__ . '.InfoField', 'The canonical domain will be added to the HTML head of your pages.  It should be specified with the full protocol and with no trailing slash, eg.  https://www.example.com').'</p>'));
-        $fields->addFieldToTab('Root.Canonical', TextField::create('CanonicalDomain')->setDescription(_t(__CLASS__ . '.CanonicalDomainDescription', 'eg. https://www.example.com')));
-        return $fields;        
+
+        $CanonicalDomainField = TextField::create('CanonicalDomain')
+            ->setDescription(_t(__CLASS__ . '.InfoField', 'The canonical domain will be added to the HTML head of your pages.'))
+            ->setAttribute('placeholder', _t(__CLASS__ . '.CanonicalDomainDescription', 'https://www.example.com'));
+
+        $fields->addFieldToTab('Root.Canonical', $CanonicalDomainField);
+
+        return $fields;
     }
 }
--- src/SiteTreeCanonicalExtension.php
+++ src/SiteTreeCanonicalExtension.php
@@ -2,43 +2,85 @@
 namespace DorsetDigital\SilverStripeCanonical;
 
 use SilverStripe\View\HTML;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\LiteralField;
 use SilverStripe\SiteConfig\SiteConfig;
 use SilverStripe\CMS\Model\SiteTreeExtension;
 
 class SiteTreeCanonicalExtension extends SiteTreeExtension
 {
+    private static $db = [
+        'CanonicalURL' => 'Text'
+    ];
+    
+    public function updateCMSFields(FieldList $fields) {
+        $MetaToggle = $fields->fieldByName('Root.Main.Metadata');
+        if ($url = $this->getorsetCanonicalURL()) {
+            $MetaToggle->push($MetaCanonical = TextField::create('CanonicalURL', _t(__CLASS__ . '.LinkOverride',"Override canonical URL")));
+            $MetaCanonical->setAttribute('placeholder', $this->getorsetCanonicalURL());
+        } else {
+            $MetaToggle->push($MetaCanonical = LiteralField::create("CanonicalURL", '<p class="form__field-label">' . _t(__CLASS__ . '.LinkFieldPlaceholder','Canonical-URLs benötigt eine Canoinical-Domain in <a href="/admin/settings">SiteConfig</a>') . '</p>'));
+        }
+        $MetaCanonical->setRightTitle(_t(__CLASS__ . '.LinkFieldRightTitle','Used to identify the original resource (URL) so that content is not considered "duplicate content". Internal & external links can be used.'));
+    }
+
+    function getorsetCanonicalURL() {
+        $siteConfig = SiteConfig::current_site_config();
+        if ($siteConfig->CanonicalDomain != '') {
+            $canonicalBase = trim($siteConfig->CanonicalDomain, '/');
+
+            // dynamic value
+            if (method_exists($this->owner, 'CanonicalLink')) {
+                $link = $this->owner->CanonicalLink();
+                $urlArray = parse_url($link);
+                if (!isset($urlArray['host']) && !isset($urlArray['scheme'])) {
+                    $canonicalBase = $urlArray['scheme'] . '://' . $urlArray['host'];
+                    $link = $canonicalBase . $link;
+                }
+            }
+
+            // canonical link on Page
+            if ($this->owner->CanonicalURL) {
+                $link = $this->owner->CanonicalURL;
+                $urlArray = parse_url($link);
+                if (!isset($urlArray['host']) && !isset($urlArray['scheme'])) {
+                    $canonicalBase = $urlArray['scheme'] . '://' . $urlArray['host'];
+                    $link = $canonicalBase . $link;
+                }
+            }
+
+            // default just link
+            if (!isset($link)) {
+                $link = $canonicalBase . $this->owner->Link();
+            }
+
+            return $link;
+        }
+    }
 
     public function MetaTags(&$tags)
     {
+        if ($canonLink = $this->getorsetCanonicalURL()) {
+            $atts = [
+                'rel' => 'canonical',
+                'href' => $canonLink
+            ];
+            $canonTag = HTML::createTag('link', $atts);
 
-    $siteConfig = SiteConfig::current_site_config();
-    if ($siteConfig->CanonicalDomain != '') {
-    $canonicalBase = trim($siteConfig->CanonicalDomain, '/');
-    if (method_exists($this->owner, 'CanonicalLink')) {
-    $link = $this->owner->CanonicalLink();
-    } else {
-    $link = $this->owner->Link();
-    }
-    $canonLink = $canonicalBase . $link;
-    $atts = [
-        'rel' => 'canonical',
-        'href' => $canonLink
-    ];
-    $canonTag = HTML::createTag('link', $atts);
+            $tagsArray = explode(PHP_EOL, $tags);
+            $tagPattern = 'rel="canonical"';
 
-    $tagsArray = explode(PHP_EOL, $tags);
-    $tagPattern = 'rel="canonical"';
+            $tagSearch = function($val) use ($tagPattern) {
+                return (stripos($val, $tagPattern) !== false ? true : false);
+            };
 
-    $tagSearch = function($val) use ($tagPattern) {
-    return (stripos($val, $tagPattern) !== false ? true : false);
-    };
+            $currentTags = array_filter($tagsArray, $tagSearch);
+            $cleanedTags = array_diff($tagsArray, $currentTags);
 
-    $currentTags = array_filter($tagsArray, $tagSearch);
-    $cleanedTags = array_diff($tagsArray, $currentTags);
+            $cleanedTags[ ] = $canonTag;
 
-    $cleanedTags[ ] = $canonTag;
-
-    $tags = implode(PHP_EOL, $cleanedTags);
-    }
+            $tags = implode(PHP_EOL, $cleanedTags);
+        }
     }
 }
